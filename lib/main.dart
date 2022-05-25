bdeba78d03970dab591fa26e97499c3ce7246e88
Muhammad Abdullahi Nma <66219684+codedprogrammer@users.noreply.github.com>
2022-05-25 06:12:09 +0100

    welcomepage completed

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/images/vigoplace.png'),
+              SizedBox(height: 40),
+              Image.asset('assets/images/userspic.png'),
+              SizedBox(height: 50),
+              Text(
+                'Build connections.',
+                style: TextStyle(
+                  color: Colors.indigo,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 7),
+              Text(
+                'Build wealth.',
+                style: TextStyle(
+                  color: Colors.indigo,
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                'Vigoplace integrates all of your personal, business and online interaction needs into one simple user - friendly application.',
+                textAlign: TextAlign.center,
+                style: TextStyle(),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: Text('Login'),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.indigo,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(15.0),
+                      // side: BorderSide(color: Colors.red),
+                    ),
+                    textStyle: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(15.0),
+                    ),
+                    side: BorderSide(
+                      width: 1.5,
+                      color: Colors.indigo,
+                    ),
+                    textStyle: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
